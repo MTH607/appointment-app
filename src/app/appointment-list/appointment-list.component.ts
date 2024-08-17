ea7b5554feabc000392e7108c9f3744423d84cbc
MTH607 <55235962+MTH607@users.noreply.github.com>
2024-08-17 19:27:36 +0200

    create form for appointments

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrl: './appointment-list.component.scss'
+  styleUrl: './appointment-list.component.scss',
 })
 export class AppointmentListComponent {
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+  appointment: Appointment[] = [];
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: 1,
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate,
+      };
+      this.appointment.push(newAppointment);
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
 
-  appointment: string = "Read a book"
+      alert(this.appointment.length);
+    }
+  }
 }
